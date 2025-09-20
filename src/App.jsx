01c25fb7bf4c29f572a672f737459d38eb3ab731
Sohail-byte <sohailijaz09@gmail.com>
2025-09-20 22:37:42 +0500

    added somethings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState, useRef, useEffect, useCallback } from "react";
+import { useState, useRef, useEffect, useCallback, use } from "react";
 import "./App.css";
 import { client } from "./lib/appwrite";
 import { AppwriteException } from "appwrite";
 import AppwriteSvg from "../public/appwrite.svg";
 import ReactSvg from "../public/react.svg";
+import Search from "./componenets/search";
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+
   return (
     <main>
       <div className="pattern"/>
       <div className="wrapper">
         <header>
+          <img src="./hero-img.svg" alt="" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
         </header>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1>{searchTerm}</h1>
       </div>
     </main>
   )
